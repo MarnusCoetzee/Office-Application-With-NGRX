77d90bc40d89e50c35c936913e1167f0d8c4a407
MarnusCoetzee <marnuscoe@gmail.com>
2020-11-17 12:10:42 +0200

    added reactive form + create office object to create-new-office-dialog

--- src/app/home/dialogs/create-new-office-dialog/create-new-office-dialog.component.ts
+++ src/app/home/dialogs/create-new-office-dialog/create-new-office-dialog.component.ts
@@ -1,10 +1,18 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { max } from 'rxjs/operators';
 import { OfficeService } from 'src/app/services/office.service';
+import { Office } from 'src/app/store/models/office.model';
 import { officeColors, Color } from '../../office-colors';
 @Component({
   selector: 'app-create-new-office-dialog',
@@ -34,8 +42,35 @@ export class CreateNewOfficeDialogComponent implements OnInit {
       officeEmailAddress: ['', Validators.required, Validators.email],
       officeTellNumber: ['', Validators.required],
       officeAddress: ['', Validators.required],
-      maxOccupants: ['', Validators.required, Validators.min(0)],
+      maxOccupants: ['', Validators.required],
     });
+  }
+
+  async onClickSaveOfficeToFirestore() {
+    const name = this.officeDetailsForm.value.officeName;
+    const email = this.officeDetailsForm.value.officeEmailAddress;
+    const location = this.officeDetailsForm.value.officeAddress;
+    const tellNumber = this.officeDetailsForm.value.tellNumber;
+    const officeColor = this.selectedColour;
+    let maxOfficeOccupants = this.officeDetailsForm.value.maxOccupants;
+    const ownerId = (await this.afAuth.currentUser).uid;
+    const id = this.db.createId();
+    const totalEmployees = 0;
+    if (maxOfficeOccupants < 0) {
+      maxOfficeOccupants = maxOfficeOccupants * -1;
+    }
+    const office: Office = {
+      name,
+      email,
+      tellNumber,
+      officeColor,
+      maxOfficeOccupants,
+      ownerId,
+      id,
+      location,
+      totalEmployees,
+    };
+    console.log(office);
   }
 
   onClickCloseDialog() {
